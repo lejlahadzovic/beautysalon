4cf8c7538c0848e1e5097368b148b523811bed59
lejlahadzovic <hadzoviclejla6@gmail.com>
2023-05-23 14:02:17 +0200

    Add validation

--- BeautySalon/Contracts/UserVM.cs
+++ BeautySalon/Contracts/UserVM.cs
@@ -1,13 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace BeautySalon.Contracts
 {
     public class UserVM
     {
-
-        [Key, Column(Order = 1)]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
@@ -17,15 +15,16 @@ namespace BeautySalon.Contracts
         [Required]
         [StringLength(50, MinimumLength = 3)]
         public string LastName { get; set; }
+
         [Required]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
         public string Email { get; set; }
 
-     
         public string PhoneNumber { get; set; }
 
         public string Gender { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
@@ -35,6 +34,9 @@ namespace BeautySalon.Contracts
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
         public string Password { get; set; }
 
-
+        [NotMapped]
+        [Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Password Not Matched")]
+        public string ConfirmPassword { get; set; }
     }
 }
---
+++ BeautySalon/Controllers/RegisterController.cs
@@ -0,0 +1,49 @@
+﻿using BeautySalon.Context;
+using BeautySalon.Contracts;
+using BeautySalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using BeautySalon.Helper;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BeautySalon.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RegisterController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserVM newUser)
+        { 
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            User user = new User();
+            _dbContext.Add(user);
+             user.FirstName = newUser.FirstName;
+                user.LastName = newUser.LastName;
+                user.Email = newUser.Email;
+                user.PhoneNumber = newUser.PhoneNumber;
+                user.Gender = newUser.Gender;
+                user.BirthDate = newUser.BirthDate;
+                user.PasswordSalt = PasswordHelper.GenerateSalt();
+                user.PasswordHash = PasswordHelper.GenerateHash(user.PasswordSalt, newUser.Password);
+                _dbContext.SaveChanges();
+          
+            return Ok();
+        }
+    }
+}
